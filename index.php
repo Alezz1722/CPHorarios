2832e9a4e809d8ff8c0aac4f4f745905b0f66758
Luis Borja <alegatito1795@hotmail.com>
2020-01-22 12:41:42 -0500

    Implementacion del panel de horarios en la pantalla principal

--- index.php
+++ index.php
@@ -1,58 +1,221 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-  <!--Fontawesome CDN-->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-  <link rel="stylesheet" type="text/css" href="css/styleIndex.css">
+    <!--Fontawesome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-  <title>Login horarios</title>
+    <link rel="stylesheet" type="text/css" href="css/styleIndex.css">
+
+    <title>Dashboard</title>
 </head>
-<body>
-  <div class="container">
-    <div class="d-flex justify-content-center h-100">
-      <div class="card">
-        <div class="card-header">
-          <h3>Login</h3>
+
+<body style="padding-top: 100px;">
+
+    <nav id="menuBar" class="navbar navbar-default navbar-fixed-top lightHeader" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Horarios de Docencia</a>
+            </div>
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class=" dropdown singleDrop">
+                        <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Docente<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <li><a href="dashboard.html">Carrion Jumbo Joe Luis</a></li>
+                            <li><a href="add-listings.html">Borja Zambrano Luis Alejandro</a></li>
+                            <li><a href="edit-listings.html">Otro Docente</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
         </div>
-        <div class="card-body">
-          <form id="formLogin">
-            <div class="input-group form-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-user"></i></span>
-              </div>
-              <input type="text" class="form-control" placeholder="username" id="username" name="username">
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <table class="table" style="background-color: white;">
+                        <thead>
+                            <tr>
+                                <th scope="col">Hora</th>
+                                <th scope="col">Lunes</th>
+                                <th scope="col">Martes</th>
+                                <th scope="col">Miercoles</th>
+                                <th scope="col">Jueves</th>
+                                <th scope="col">Viernes</th>
+                                <th scope="col">Sabado</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>07-08</td>
+                                <td></td><!-- Lunes -->
+                                <td>Fisica</td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>08-09</td>
+                                <td></td><!-- Lunes -->
+                                <td>Fisica</td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>09-10</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+
+                            <tr>
+                                <td>10-11</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+
+                            <tr>
+                                <td>11-12</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>12-13</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td style="background-color:blueviolet;color:white">Libre</td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>13-14</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td style="background-color:blueviolet;color:white">Libre</td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>14-15</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>15-16</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>16-17</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>17-18</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>18-19</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>19-20</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>20-21</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                            <tr>
+                                <td>21-22</td>
+                                <td></td><!-- Lunes -->
+                                <td></td><!-- Martes -->
+                                <td></td><!-- Miercoles -->
+                                <td></td><!-- Jueves -->
+                                <td></td><!-- Viernes -->
+                                <td></td><!-- Sabado -->
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="input-group form-group">
-              <div class="input-group-prepend">
-                <span class="input-group-text"><i class="fas fa-key"></i></span>
-              </div>
-              <input type="password" class="form-control" placeholder="password" id="password" name="password">
-            </div>
-            <div class="form-group">
-              <input type="submit" value="Ingresar" class="btn float-right login_btn">
-            </div>
-          </form>
         </div>
-      </div>
     </div>
-  </div>
 
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-  <script type="text/javascript" src="js/jquery.js"></script>
-  <script type="text/javascript" src="js/jquery.validate.js"></script>
-  <script type="text/javascript" src="js/additional-methods.js"></script>
-  <script type="text/javascript" src="js/validaciones/validaLogin.js"></script>
-  <script type="text/javascript" src="js/login.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.js"></script>
+    <script type="text/javascript" src="js/additional-methods.js"></script>
+    <script type="text/javascript" src="js/validaciones/validaLogin.js"></script>
+    <script type="text/javascript" src="js/login.js"></script>
 </body>
+
 </html>
